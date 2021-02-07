aea66ef526ee08fb5890873d2766e121bfe42471
Gitapim <57094137+Gitapim@users.noreply.github.com>
2021-02-07 19:37:45 +0100

    Add files via upload

---
+++ part01-Part01_09.Conversation/src/main/java/Conversation.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Conversation {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Greetings! How are you doing?");
+        reader.nextLine();
+        System.out.println("Oh, how interesting. Tell me more!");
+        reader.nextLine();
+        System.out.println("Thanks for sharing!");
+
+    }
+}
